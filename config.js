a5384f85cc133c944052c9c9516cfb11053042cd
ASRASHRAF245 <gashrf083@gmail.com>
2025-04-04 16:42:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -16,11 +16,11 @@ global.isBaileysFail = false
 global.defaultLenguaje = 'es';
 
 global.owner = [
-  ['201151094460', '👑 ERIN-MD 👑', true],
+  ['201151094460', '👑 .GOKAR-MD 👑', true],
 ];
 
-global.suittag = ['201151094460'];
-global.prems = ['201151094460'];
+global.suittag = ['201011547182'];
+global.prems = ['201030543290'];
 
 // API
 global.BASE_API_DELIRIUS = "https://delirius-apiofc.vercel.app";
